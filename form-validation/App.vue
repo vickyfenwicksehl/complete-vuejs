28d2539e3a20832fcd6a6c1940e9b1062e779b8d
Vicky Fenwick-Sehl <vicky@campbrain.com>
2022-02-04 18:03:13 -0500

    form-validation mini-project

---
+++ form-validation/App.vue
@@ -0,0 +1,73 @@
+<template>
+<form @submit.prevent="submit">
+<my-input 
+name="Username"
+:rules="{ required: true, min: 5}"
+:value="username.value"
+:error="username.error"
+@update="update"
+/>
+<my-input 
+name="Password"
+:rules="{ required: true, min: 10}"
+:value="password.value"
+:error="password.error"
+type="password"
+@update="update"
+/>
+  <my-button
+    background="darkslateblue"
+    color="white"
+    :disabled="!valid"
+   />
+</form>
+</template>
+
+<script>
+import MyButton from './MyButton.vue'
+import MyInput from '../wedding-seating-app/MyInput.vue'
+export default{
+  components:{
+    MyButton,
+    MyInput
+  },
+
+data(){
+  return{
+    valid: true,
+    username:{
+      value:'user',
+      error: ''
+    },
+    password:{
+      value:'pass',
+      error: ''
+    }
+  }
+},
+
+computed:{
+  valid(){
+    return(
+      !this.username.error &&
+      !this.password.error
+    )
+  }
+},
+
+methods:{
+  submit(){
+    console.log('Event')
+  },
+  update({name , value, error}){
+    this[name].value = value
+    this[name].error=error
+  }
+}
+
+
+}
+</script>
+
+<style scoped>
+</style>
